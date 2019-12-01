51f1aaa31568e6b0f5396c5650e5c231e6e3546c
snisioi <sergiu.nisioi@oracle.com>
2019-12-01 11:46:17 +0200

    update start script

--- start_cocospa.sh
+++ start_cocospa.sh
@@ -10,4 +10,4 @@ DBPEDIA_IN_NEO4J="${DATA_DIR}/dbpediaNeo4J"
 DBPEDIA_IN_HDT=${DATA_DIR}/DBpedia2014Selected.hdt
 
 # Run the spring service
-mvn spring-boot:run -Dspring-boot.run.arguments="${DBPEDIA_IN_NEO4J},${DBPEDIA_IN_HDT},127.0.0.1,6379,${DBSPOTLIGHT_URL}"
+mvn spring-boot:run -Dspring-boot.run.arguments="${DBPEDIA_IN_NEO4J},${DBPEDIA_IN_HDT},127.0.0.1,6379,${DBSPOTLIGHT_URL}/annotate"
